ad0576ec8bf03c5afb2e6b1397514cbd011db0bd
Jason A. Smith <jas7553@users.noreply.github.com>
2024-05-26 17:48:35 +0200

    Enforce that bets cannot equal the round number

--- src/components/EnterBets.tsx
+++ src/components/EnterBets.tsx
@@ -17,6 +17,10 @@ export default function EnterBets(props: {
     ),
   );
 
+  const betsAreEqualToTheRoundNumber =
+    Object.values(bets).reduce((sum, betCount) => sum + betCount, 0) ===
+    props.rounds.length;
+
   return (
     <>
       <header>
@@ -32,7 +36,15 @@ export default function EnterBets(props: {
       />
       <div>
         <button onClick={props.onBack}>Back</button>
-        <button onClick={() => props.onConfirmBets(bets)}>Confirm Bets</button>
+        <button
+          disabled={betsAreEqualToTheRoundNumber}
+          onClick={() => props.onConfirmBets(bets)}
+        >
+          Confirm Bets
+        </button>
+        {betsAreEqualToTheRoundNumber
+          ? "(bets cannot equal the round number)"
+          : null}
       </div>
       <hr />
       <ScoreSheet
